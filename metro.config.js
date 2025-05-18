d2e6147dafeed611a275d77f2da62848d5656fd4
James Caldwell <jcaldwell2@radford.edu>
2025-05-18 15:57:34 -0400

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 // Get default Expo config
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
 // Optional: Add file extensions if you use more than standard ones
