67197439663a5bfa27a610f44a075c53ba9b29e6
James Caldwell <jcaldwell2@radford.edu>
2025-05-18 17:03:06 -0400

    Fix Metro configuration and dependencies

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,9 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
 
 module.exports = config;
